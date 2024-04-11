b4659b0e8e5a86e05b982da3a71c21b8683a9139
Violet Iapalucci <violet_iapalucci@outlook.com>
2024-04-11 09:05:06 -0400

    Initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,37 @@
 use darling_api as darling;
 
-// You can rename this safely
-pub struct MyPackageManager;
+pub struct Cargo;
 
-// Do not rename this! it must be `pub static PACKAGE_MANAGER` and impl `darling::PackageManager`.
-pub static PACKAGE_MANAGER: MyPackageManager = MyPackageManager;
+pub static PACKAGE_MANAGER: Cargo = Cargo;
 
-impl darling::PackageManager for MyPackageManager {
+impl darling::PackageManager for Cargo {
     fn name(&self) -> String {
-        unimplemented!()
+        "cargo".to_owned()
     }
 
-    fn install(&self, context: &darling::Context, package: &darling::InstallationEntry) -> anyhow::Result<()> {
-        unimplemented!();
+    fn install(&self, _context: &darling::Context, package: &darling::InstallationEntry) -> anyhow::Result<()> {
+        std::process::Command::new("cargo").arg("install").arg(&package.name).spawn()?.wait()?;
+        Ok(())
     }
 
-    fn uninstall(&self, context: &darling::Context, package: &darling::InstallationEntry) -> anyhow::Result<()> {
-        unimplemented!();
+    fn uninstall(&self, _context: &darling::Context, package: &darling::InstallationEntry) -> anyhow::Result<()> {
+        std::process::Command::new("cargo").arg("uninstall").arg(&package.name).spawn()?.wait()?;
+        Ok(())
     }
 
-    fn get_all_explicit(&self, context: &darling::Context) -> anyhow::Result<Vec<(String, String)>> {
-        unimplemented!();
+    fn get_all_explicit(&self, _context: &darling::Context) -> anyhow::Result<Vec<(String, String)>> {
+        let output = String::from_utf8(std::process::Command::new("cargo").arg("install").arg("--list").output()?.stdout)?;
+        let installed_crates = output.lines().filter(|line| !line.chars().all(|char| char.is_whitespace()));
+        let pattern = regex_macro::regex!(r"^(\S+)\s(^:)");
+        let crates = installed_crates
+            .map(|entry| {
+                let captures = pattern
+                    .captures(entry)
+                    .ok_or_else(|| anyhow::anyhow!("Error parsing installed crate: {entry}"))?;
+                Ok((captures[1].to_owned(), captures[2].to_owned()))
+            })
+            .collect::<anyhow::Result<Vec<_>>>()?;
+
+        Ok(crates)
     }
 }
